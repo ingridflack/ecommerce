d2e4231a7045842bdd21cb7b150fb8bdf94acb31
ingridflack <ingrid.flack@newtail.com.br>
2022-06-15 10:30:59 -0300

    feat: Project routes created

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import theme from "../styles/theme";
+import { GlobalStyle } from "../styles/globals";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
+
       <Component {...pageProps} />
     </ThemeProvider>
   );
